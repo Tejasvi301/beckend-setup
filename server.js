28a8b73a16aacdbfed612e9da16f87b2c87cb4bf
Tejasvi <tejasvigupta77@gmail.com>
2021-08-27 08:41:59 -0700

    added

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ var corsOptions = {
 app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
